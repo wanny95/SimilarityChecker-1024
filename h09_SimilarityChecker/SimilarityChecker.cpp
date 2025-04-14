c0721b79d7075423772fc7b99e98d88c80400fec
wanny <wankims@gmail.com>
2025-04-14 09:43:50 +0900

    add feature length

--- h09_SimilarityChecker/SimilarityChecker.cpp
+++ h09_SimilarityChecker/SimilarityChecker.cpp
@@ -1,5 +1,33 @@
+#include <string>
+
+using std::string;
 
 class SimilarityChecker {
 public:
+	int getLengthScore(string firstStr, string secondStr) {
+		int firstStrLength = firstStr.length();
+		int secondStrLength = secondStr.length();
 
+		double gap = 0;
+		int longStrLength = 0;
+		int shortStrLength = 0;
+
+		if (firstStrLength >= secondStrLength) {
+			gap = firstStrLength - secondStrLength;
+			longStrLength = firstStrLength;
+			shortStrLength = secondStrLength;
+		}
+		else {
+			gap = secondStrLength - firstStrLength;
+			longStrLength = secondStrLength;
+			shortStrLength = firstStrLength;
+		}
+
+		if (longStrLength >= shortStrLength * 2)
+			return 0;
+
+		int result = (1 - (gap / shortStrLength)) * 60;
+		result = result >= 0 ? result : 0;
+		return result;
+	}
 };
